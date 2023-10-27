3f62abfef9429862d8b5fbf2b015d8460d00a50a
Victor Nieves <c_cc_vic@yahoo.com>
2023-10-27 11:01:28 -0400

    created all routes for tag-routes file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-
+// The `/api/categories` endpoint
 router.get('/', (req, res) => {
   Category.findAll().then(categories => {
     res.json(categories);
   }).catch(err => res.status(500).json(err));
 });
 
+// Category get route by id
 router.get('/:id', (req, res) => {
   Category.findOne({
     where:{
@@ -19,22 +20,22 @@ router.get('/:id', (req, res) => {
 
  // create a new category
 router.post('/', (req, res) => {
-  const { name } = req.body;
+  const { category_name } = req.body;
   Category.create({
-    name: name,
+    name: category_name,
   })
   .then(category => res.json(category))
   .catch(err => res.status(500).json(err));
 });
 
-// add a new category
+// Update a category
 router.put('/:id', (req, res) => {
   const categoryId = req.params.id;
-  const { name } = req.body;
+  const { category_name } = req.body;
 
   Category.findByPk(categoryId)
   .then(category => {
-    category.name = name;
+    category.name = category_name;
     return category.save();
   })
   .then(category => res.json(category))
@@ -52,7 +53,7 @@ router.delete('/:id', (req, res) => {
       res.status(204).send(); 
     })
     .catch((err) => {
-      console.error('Error deleing category: ', err);
+      console.error('Error deleting category: ', err);
       res.status(500).json({err: 'Internal Server Error' });
     });
   
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,28 +1,62 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+
 // The `/api/tags` endpoint
-
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll().then(tags => {
+    res.json(tags);
+  }).catch(err => res.status(500).json(err));
 });
 
+// Find a single tag by its `id`
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id: req.params.id
+    }
+  }).then(tag => res.json(tag))
+  .catch(err => res.status(500).json(err));
 });
 
+// Create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  const { tag_name } = req.body;
+  Tag.create({
+    name: tag_name,
+  })
+  .then(tag => res.json(tag))
+  .catch(err => res.status(500).json(err));
 });
 
+// Update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  const categoryId = req.params.id;
+  const { tag_name } = req.body;
+
+  Tag.findByPk(tagId)
+  .then(tag => {
+    tag.name = tag_name;
+    return tag.save();
+  })
+  .then(tag => res.json(tag))
+  .catch(err => res.status(500).json(err));
 });
 
+// delete tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  const tagId = req.params.id;
+
+  Tag.destroy({
+    where: { id: tagId }
+  })
+    .then(() => {
+      res.status(204).send(); 
+    })
+    .catch((err) => {
+      console.error('Error deleting tag: ', err);
+      res.status(500).json({err: 'Internal Server Error' });
+    });
 });
 
 module.exports = router;
